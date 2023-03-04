df277b7121a7087db9ace85942c67982076ebf71
Tirsh <valeriy.vasipov@gmail.com>
2023-03-04 18:51:58 +0300

    Created Controller class with main methods

--- src/main/java/com/tirsh/toy_lottery/Main.java
+++ src/main/java/com/tirsh/toy_lottery/Main.java
@@ -1,12 +1,16 @@
 package com.tirsh.toy_lottery;
 
 import com.tirsh.toy_lottery.controller.Controller;
-import com.tirsh.toy_lottery.dao.DaoToyController;
+import com.tirsh.toy_lottery.model.Toy;
 
 public class Main {
     public static void main(String[] args) {
         Controller controller = new Controller();
+//        Toy new_toy = new Toy("Crocodile", 2, 15);
+//        controller.add(new_toy);
+//        controller.delete(5);
         var data = controller.getAllToys();
         System.out.println(data);
+        System.out.println(controller.getToy(3));
     }
 }
--- src/main/java/com/tirsh/toy_lottery/controller/Controller.java
+++ src/main/java/com/tirsh/toy_lottery/controller/Controller.java
@@ -2,7 +2,6 @@ package com.tirsh.toy_lottery.controller;
 
 import com.tirsh.toy_lottery.dao.DaoToyController;
 import com.tirsh.toy_lottery.model.Toy;
-import com.tirsh.toy_lottery.util.DBConnection;
 
 import java.sql.SQLException;
 import java.util.List;
@@ -17,6 +16,23 @@ public class Controller {
     public List<Toy> getAllToys() {
         try {
             return daoToyController.getAll();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public Toy add(Toy toy) {
+        return daoToyController.save(toy);
+    }
+
+    public boolean delete(int id){
+        return daoToyController.delete(id);
+    }
+
+    public Toy getToy(int id){
+        try {
+            return daoToyController.getById(id);
         } catch (SQLException e) {
             e.printStackTrace();
             return null;

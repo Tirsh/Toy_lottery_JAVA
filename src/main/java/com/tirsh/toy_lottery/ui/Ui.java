1c8ce5e39a714b6c43b1225c7f9cc6f18fafde30
Tirsh <valeriy.vasipov@gmail.com>
2023-03-05 20:27:00 +0300

    UI + add and show all

--- src/main/java/com/tirsh/toy_lottery/ui/Ui.java
+++ src/main/java/com/tirsh/toy_lottery/ui/Ui.java
@@ -12,19 +12,14 @@ import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 
-import static java.awt.Component.LEFT_ALIGNMENT;
-
 public class Ui {
-    private DataController dataController;
-    private LotteryGame lotteryGame;
-    private Logger logger;
+    private final DataController dataController;
     private JFrame toyLotteryFrame;
     private DefaultListModel<Toy> currentList, lotteryList;
-    private JButton newLotteryButton, startLotteryButton, addButton, removeButton;
-    private JList jList, jListSelected;
-    private JLabel toys_Label, selectedToysLabel;
-    private JDialog jDialog;
-    Container contentpane;
+    private JButton newLotteryButton, startLotteryButton, addButton, removeButton, backFromLotteryButton, addToyButton, allToyButton;
+    private JList<Toy> jList, jListSelected;
+    private JLabel toysLabel, selectedToysLabel;
+    private JDialog jDialog, addNewDialog;
 
     public Ui(DataController dataController) {
         this.dataController = dataController;
@@ -33,20 +28,34 @@ public class Ui {
 
     private void initFrame() {
         toyLotteryFrame = new JFrame("Toys lottery");
+
         newLotteryButton = new JButton("Новая Лотерея");
-        newLotteryButton.setBounds(50, 100, 200, 30);
+        newLotteryButton.setBounds(50, 80, 200, 30);
+        addToyButton = new JButton("Добавить игрушку");
+        addToyButton.setBounds(50, 120, 200, 30);
+        allToyButton = new JButton("Показать игрушки");
+        allToyButton.setBounds(50, 160, 200, 30);
         toyLotteryFrame.add(newLotteryButton);
+        toyLotteryFrame.add(addToyButton);
+        toyLotteryFrame.add(allToyButton);
+
         toyLotteryFrame.setSize(320, 300);
         toyLotteryFrame.setLayout(null);
         toyLotteryFrame.setVisible(true);
+
+        addToyButton.setActionCommand("addNewToy");
+        addToyButton.addActionListener(new ButtonClickListener());
+//        allToyButton.setActionCommand("showAll");
         newLotteryButton.setActionCommand("newLottery");
+
         newLotteryButton.addActionListener(new ButtonClickListener());
+
         toyLotteryFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
     private void chooseToysFrame() {
-        toys_Label = new JLabel("Список игрушек:");
-        toys_Label.setBounds(20, 00, 105, 40);
+        toysLabel = new JLabel("Список игрушек:");
+        toysLabel.setBounds(20, 0, 105, 40);
 
         selectedToysLabel = new JLabel("Выбраны:");
         selectedToysLabel.setBounds(180, 0, 105, 40);
@@ -55,7 +64,7 @@ public class Ui {
         lotteryList = new DefaultListModel<>();
         currentList.addAll(dataController.getAllToys());
 
-        jList = new JList(currentList);
+        jList = new JList<>(currentList);
         jListSelected = new JList<>(lotteryList);
 
         jList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
@@ -76,18 +85,23 @@ public class Ui {
         addButton.addActionListener(new ButtonClickListener());
         removeButton.setActionCommand("removeToy");
         removeButton.addActionListener(new ButtonClickListener());
-        removeButton.addActionListener(new ButtonClickListener());
 
-        startLotteryButton = new JButton("Начать розыгрышь");
-        startLotteryButton.setBounds(50, 220, 200, 30);
+        startLotteryButton = new JButton("Начать розыгрыш");
+        startLotteryButton.setBounds(15, 220, 180, 30);
         startLotteryButton.setActionCommand("startLottery");
         startLotteryButton.addActionListener(new ButtonClickListener());
 
-        toyLotteryFrame.add(toys_Label);
+        backFromLotteryButton = new JButton("Назад");
+        backFromLotteryButton.setBounds(210, 220, 80, 30);
+        backFromLotteryButton.setActionCommand("menu");
+        backFromLotteryButton.addActionListener(new ButtonClickListener());
+
+        toyLotteryFrame.add(toysLabel);
         toyLotteryFrame.add(selectedToysLabel);
         toyLotteryFrame.add(jList);
         toyLotteryFrame.add(jListSelected);
         toyLotteryFrame.add(startLotteryButton);
+        toyLotteryFrame.add(backFromLotteryButton);
         toyLotteryFrame.add(addButton);
         toyLotteryFrame.add(removeButton);
 
@@ -116,14 +130,17 @@ public class Ui {
 
     private void startLottery(){
         var toys = jListSelected.getModel();
+        if (toys.getSize() == 0) {
+            return;
+        }
         List<Toy> toysList= new ArrayList<>();
         for (int i = 0; i < toys.getSize(); i++){
-            toysList.add(((Toy)toys.getElementAt(i)));
+            toysList.add(toys.getElementAt(i));
         }
-        lotteryGame = new LotteryGame(toysList);
+        LotteryGame lotteryGame = new LotteryGame(toysList);
         Toy chosenToy = lotteryGame.priceDrawing();
         dataController.decreaseQuantity(chosenToy);
-        logger = new Logger(chosenToy);
+        Logger logger = new Logger(chosenToy);
         logger.writeDataToCSV();
         currentList.removeAllElements();
         lotteryList.removeAllElements();
@@ -136,13 +153,7 @@ public class Ui {
         jDialog = new JDialog(toyLotteryFrame, "Розыгрыш состоялся", true);
         jDialog.setLayout( new FlowLayout() );
         JButton b = new JButton ("OK");
-        b.addActionListener ( new ActionListener()
-        {
-            public void actionPerformed( ActionEvent e )
-            {
-                jDialog.setVisible(false);
-            }
-        });
+        b.addActionListener (e -> jDialog.setVisible(false));
         jDialog.add( new JLabel ("Выпала игрушка: %s.".formatted(toy)));
         jDialog.add( new JLabel ("Информация о розыгрыше сохранена в draws.csv"));
         jDialog.add(b);
@@ -150,27 +161,64 @@ public class Ui {
         jDialog.setVisible(true);
     }
 
+    private void createAddDialog(){
+        addNewDialog = new JDialog(toyLotteryFrame, "Добавить новую игрушку", true);
+        addNewDialog.setLayout( new FlowLayout() );
+        JTextField title, quantity, dropFrequency;
+        title=new JTextField();
+        title.setBounds(50,100, 200,30);
+        quantity=new JTextField();
+        quantity.setBounds(50,150, 200,30);
+        dropFrequency=new JTextField();
+        dropFrequency.setBounds(50,200, 200,30);
+        addNewDialog.add(title);
+        addNewDialog.add(quantity);
+        addNewDialog.add(dropFrequency);
+        JButton b = new JButton ("OK");
+        b.addActionListener (e -> addNewDialog.setVisible(false));
+        addNewDialog.add(b);
+        addNewDialog.setSize(320,300);
+        addNewDialog.setVisible(true);
+    }
+//
+//    private void showAllDialog(){
+//        showAllDialog = new JDialog(toyLotteryFrame, "Список игрушек", true);
+//        showAllDialog.setLayout( new FlowLayout() );
+//        JButton b = new JButton ("OK");
+//        b.addActionListener (e -> showAllDialog.setVisible(false));
+//        showAllDialog.add(b);
+//        showAllDialog.setSize(320,3000);
+//        showAllDialog.setVisible(true);
+//    }
+
     private class ButtonClickListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
             String command = e.getActionCommand();
             switch (command) {
-                case "newLottery": {
+                case "newLottery" -> {
                     newLotteryButton.setVisible(false);
+                    addToyButton.setVisible(false);
+                    allToyButton.setVisible(false);
                     chooseToysFrame();
-                    break;
                 }
-                case "addToy": {
-                    addToy();
-                    break;
-                }
-                case "removeToy": {
-                    removeToy();
-                    break;
-                }
-                case "startLottery": {
-                    startLottery();
-                    break;
+                case "addToy" -> addToy();
+                case "removeToy" -> removeToy();
+                case "startLottery" -> startLottery();
+                case "addNewToy" -> createAddDialog();
+//                case "showAll" -> showAllDialog();
+                case "menu" -> {
+                    toysLabel.setVisible(false);
+                    selectedToysLabel.setVisible(false);
+                    addButton.setVisible(false);
+                    removeButton.setVisible(false);
+                    jList.setVisible(false);
+                    jListSelected.setVisible(false);
+                    startLotteryButton.setVisible(false);
+                    backFromLotteryButton.setVisible(false);
+//                    addToyButton.setVisible(true);
+//                    allToyButton.setVisible(true);
+                    newLotteryButton.setVisible(true);
                 }
             }
         }

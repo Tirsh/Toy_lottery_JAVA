ced4cd6e13fbd473534e041f68daa1a5d2766c5f
Tirsh <valeriy.vasipov@gmail.com>
2023-03-06 06:31:54 +0300

    Final commit fixed

--- src/main/java/com/tirsh/toy_lottery/ui/Ui.java
+++ src/main/java/com/tirsh/toy_lottery/ui/Ui.java
@@ -169,6 +169,9 @@ public class Ui {
     }
 
     private void createAddDialog(Toy toy) {
+        if (toy == null) {
+            toy = new Toy(null,null, 0, 0);
+        }
         addNewDialog = new JDialog(toyLotteryFrame, "Добавить новую игрушку", true);
         addNewDialog.setLayout(new FlowLayout());
 
@@ -185,9 +188,10 @@ public class Ui {
         addNewDialog.add(new JLabel("Введите вероятность:"));
         addNewDialog.add(dropFrequency);
         JButton submitButton = new JButton("OK");
+        Toy finalToy = toy;
         submitButton.addActionListener(e -> {
             if (title.getText().length() > 0 && isInt(quantity.getText()) && isInt(dropFrequency.getText())) {
-                dataController.save(new Toy(toy.getId(), title.getText(),
+                dataController.save(new Toy(finalToy.getId(), title.getText(),
                         Integer.parseInt(quantity.getText()), Integer.parseInt(dropFrequency.getText())));
             }
             addNewDialog.setVisible(false);
